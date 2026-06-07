957dd498ba92d095c49ea8c966c4ad3ba1be2419
Rohit62999 <mr.dark00112@gmail.com>
2026-06-07 10:37:31 +0530

    React-router

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+import { data } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData();
+    // const [data,setData]=useState([])
+    // useEffect(()=>{
+    //    fetch('https://api.github.com/users/Rohit62999')
+    //    .then(Response=>Response.json())
+    //    .then(data=> {
+    //     console.log(data);
+    //     setData(data)
+    //    })
+    // },[])
+  return (
+    <div className='text-center m-4 bg-gray-400 text-white p-5 text-3xl'>Github followers:{data.followers}
+    <img src={data.avatar_url} alt="Git Picture" width={300}/>
+    </div>
+    
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async()=>{
+    const response = await fetch('https://api.github.com/users/Rohit62999')
+    return response.json()
+}
